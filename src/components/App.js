1a7611ef38db6f5ca79e155deabcddceb5077980
Kyle PC <kyle.dahl201@gmail.com>
2020-05-19 21:51:32 -0500

    app.js

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class App extends React.Component {
+    render() {
+            return(
+                <div className="bloom-of-the-day">
+                    <p>hello world</p>
+                </div>
+            )
+
+
+    }
+
+
+
+}
+
+
+export default App;
