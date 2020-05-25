ef017bba9c1a97173e88fb2c5c380605cb37b503
Kyle PC <kyle.dahl201@gmail.com>
2020-05-25 14:18:41 -0500

    monster overhaul

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,11 +1,18 @@
 import React from "react";
+import {Redirect, Link} from "react-router-dom"
+
 
 //my garden component will be updated with add plant
 class search extends React.Component {
 
     render() {
         return (
-            <div className="search">Search!</div>
+            <div className="search">Search/main page!
+
+                <br/>
+            <Link to="/mygarden">My Garden</Link>
+
+            </div>
            
         );
     }
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,11 +1,23 @@
 import React from "react";
+import {Redirect, Link} from "react-router-dom"
 
 //my garden component will be updated with add plant
 class SignUp extends React.Component {
 
+    
+
     render() {
         return (
-            <div className="SignUp">Sign up!</div>
+            <div className="SignUp">Sign up page!
+            
+            
+            {/* <Redirect to="/search" /> */}
+
+            <br/>
+
+            <Link to="/search">Main Page</Link>
+
+            </div>
            
         );
     }
