063b20c52aca6e32f908454988ec8d70e17fcd3d
Kyle PC <kyle.dahl201@gmail.com>
2020-05-25 17:57:17 -0500

    add search bar to search component

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -9,6 +9,15 @@ class search extends React.Component {
         return (
             <div className="search">Search/main page!
 
+            <br/>
+
+            {/* <!-- Search form --> */}
+            <form className="form-group mt-5 mb-5">
+        <input className="form-control mb-5" required  placeholder="Plant Name" />
+        <button className="btn btn-success mt-3 mb-5" type="submit">
+          Search
+        </button>
+      </form>
                 <br/>
             <Link to="/mygarden">My Garden</Link>
 
