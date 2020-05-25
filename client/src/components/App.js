6f03d9d24cfbd37e3a2f3956b0f2ae1b13a95c8e
Kyle PC <kyle.dahl201@gmail.com>
2020-05-24 20:35:10 -0500

    style changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
     }
     render() {
             return(
-                <div className="catch-of-the-day">
+                <div className="plants-of-the-day">
                     <div className="menu">
                         <Header tagline="Fresh Plants Daily" />
                         
