82e4f54e702e2548934b5280a9fc4b560dde9ec8
Kyle PC <kyle.dahl201@gmail.com>
2020-05-25 22:03:11 -0500

    Add user form, sign up work

---
+++ client/src/components/AddUserForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {Redirect, Link} from "react-router-dom"
+
+
+//my garden component will be updated with add plant
+class AddUserForm extends React.Component {
+
+    
+
+    render() {
+        return (
+            <div className="AddUserForm">
+            
+            
+            
+           
+
+          
+
+            </div>
+           
+        );
+    }
+
+}  
+
+export default AddUserForm;
