f5611eeed26ad72083594dc6d2028b39c038ba5b
fruityvegetables <gg.troyer@gmail.com>
2020-05-26 11:03:06 -0500

    added grid & jumbotron components

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,22 +1,22 @@
 import React from "react";
 import {Redirect, Link} from "react-router-dom"
-import axios from "axios";
-import {search} from "./Search.js";
-require('dotenv').config();
+// import axios from "axios";
+// import {search} from "./Search.js";
+// require('dotenv').config();
 
 
 class SearchResults extends React.Component {
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~TREFLE SEARCH
-    componentDidMount() {
-        var userQuery = ({search}).val().trim();
-        var trefleUrl = 'https://cors-anywhere.herokuapp.com/https://trefle.io/api/plants?q=' + userQuery + '&token=' + process.env.REACT_APP_TREFLE_KEY;
-        axios.get(trefleUrl)
-          .then(res => {
-            const plants = res.data;
-            this.setState({ plants });
-          })
-      }
+    // componentDidMount() {
+    //     var userQuery = ({search}).val().trim();
+    //     var trefleUrl = 'https://cors-anywhere.herokuapp.com/https://trefle.io/api/plants?q=' + userQuery + '&token=' + process.env.REACT_APP_TREFLE_KEY;
+    //     axios.get(trefleUrl)
+    //       .then(res => {
+    //         const plants = res.data;
+    //         this.setState({ plants });
+    //       })
+    //   }
    
     render() {
         return (
